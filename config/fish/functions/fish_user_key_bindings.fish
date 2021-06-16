ffa83681f82adbdcbeed62b7ed31345e07fdbb05
SavageMessiah <cardboard42@gmail.com>
2021-06-16 02:48:40 -0400

    fish updates

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,22 +1,4 @@
-function bind_bang
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
-end
-
-function bind_dollar
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
-end
-
+# Defined in /home/ken/.config/fish/functions/fish_user_key_bindings.fish @ line 2
 function fish_user_key_bindings
   fzf_key_bindings
   fish_vi_key_bindings insert
@@ -24,7 +6,7 @@ function fish_user_key_bindings
   bind -M insert \cf forward-char
   bind -M insert \cp up-or-search
   bind -M insert \cn down-or-search
-
-  bind -M insert ! bind_bang
-  bind -M insert '$' bind_dollar
+  bind -M insert \ck history-token-search-backward
+  bind -M insert \cl nextd-or-forward-word
+  bind -M insert \ch prevd-or-backward-word
 end
